1e908c724211c3404c291800172ab0cf7be8952e
Tomesyy <adetomiwaadesanya01@gmail.com>
2020-01-27 15:50:36 +0100

    write complete monolith function

---
+++ main.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+let args = process.argv;
+let [node, main, ...files ] = args;
+
+files.forEach(file => {
+    let filePath = path.join(__dirname + "/" + file);
+    fs.exists(filePath, (exists) => {
+        if(exists){
+            createError(`${file} already exists in path`);
+        }
+        fs.writeFile(filePath, "", (err) => {
+            if(err){
+                createError('problem_creating_file');
+            }
+        })
+    })
+})
+
+function createError(message){
+    let error = new Error();
+    error.message = message;
+    throw error;
+}
